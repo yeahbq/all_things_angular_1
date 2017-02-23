683c60e4ef562a65235d227c7f8c040dfe1cc577
Barrett Quan <21barrett@gmail.com>
2017-02-23 15:34:45 -0800

    uncovered presidents, need to add strike through toggle class

--- angular_http/starter_code/public/controllers/presidentsController.js
+++ angular_http/starter_code/public/controllers/presidentsController.js
@@ -1,3 +1,4 @@
+//most of code will go here
 (function() {
   "use strict";
 
@@ -5,22 +6,74 @@
     .module('ThePresidentsApp')
     .controller('PresidentsController', PresidentsController);
 
-  PresidentsController.$inject = [];
+  PresidentsController.$inject = ['$http'];
 
-  function PresidentsController(){
+  function PresidentsController($http){
     var vm = this;
-    vm.all = [
-      {"name": "Blorp Florp McRichards", "start": 1789, "end": 1790 },
-      {"name": "John MuscleBrain Adams", "start": 1790, "end": 1801 },
-      {"name": "Blogpost Dorgabn", "start": 1801, "end": 1949 },
-      {"name": "Mike", "start": 1949, "end": 1947 }
-    ];
+    // vm.all = [
+    //   {"name": "Blorp Florp McRichards", "start": 1789, "end": 1790 },
+    //   {"name": "John MuscleBrain Adams", "start": 1790, "end": 1801 },
+    //   {"name": "Blogpost Dorgabn", "start": 1801, "end": 1949 },
+    //   {"name": "Mike", "start": 1949, "end": 1947 }
+    // ];
+    vm.all = [];
     vm.addPresident = addPresident;
     vm.newPresident = {};
+    vm.truth = true;
+    vm.removePresident = removePresident;
+    vm.uncover = uncover;
+
+    function addClass(president) {
+      if (president.uncovered === true) {
+        debugger;
+      }
+    }
+
+    function uncover(president) {
+      let url = '/api/presidents/' + president._id;
+      president.uncovered = !president.uncovered;
+      // addClass(president)
+      $http
+        .put(url, president)
+        .then(function(response) {
+          getPresidents();
+        }, function(err) {
+            console.log(err)
+        })
+    }
+
+    function removePresident(president){
+      let url = '/api/presidents/' + president._id;
+        $http
+          .delete(url)
+          .then(function(response){
+            getPresidents();
+        }, function(err) {
+              console.log(err);
+        });
+      }
+
+    function getPresidents(){
+        $http
+          .get('/api/presidents')
+          .then(function(response){
+            vm.all = response.data.presidents;
+        }, function(err) {
+              console.log(err);
+        });
+      }
+
+  getPresidents();
 
     function addPresident(){
-      vm.all.push(vm.newPresident);
-      vm.newPresident = {};
-    }
+        $http
+        .post('/api/presidents', vm.newPresident)
+        .then(function(res) {
+          vm.all.push(res.data.president);
+          vm.newPresident = {};
+        }, function(err) {
+          console.log(err);
+        });
+      }
   }
 })();
